6745166323e533c70be49dc97fef4d2048ba9f10
Felipe <Felipe@FAMILIA-MORENO-GALVEZ>
2024-09-22 16:55:22 -0500

    Consola

--- MercadoMayorista/src/co/edu/unbosque/view/ViewFacade.java
+++ MercadoMayorista/src/co/edu/unbosque/view/ViewFacade.java
@@ -3,18 +3,27 @@ package co.edu.unbosque.view;
 public class ViewFacade {
 
 	VentanaInicio vi;
+<<<<<<< HEAD
 	VentanaCRUD vc;
 	VentanaCrear vcr;
+=======
+	VentanaCrearProducto vc;
+>>>>>>> branch 'main' of https://github.com/EllDanii/MercadoMayorista.git
 
 	public ViewFacade() {
 		vi = new VentanaInicio();
+<<<<<<< HEAD
 		vc = new VentanaCRUD();
 		vcr = new VentanaCrear();
+=======
+		vc= new VentanaCrearProducto();
+>>>>>>> branch 'main' of https://github.com/EllDanii/MercadoMayorista.git
 	}
 
 	public VentanaInicio getVi() {
 		return vi;
 	}
+<<<<<<< HEAD
 	
 	public VentanaCRUD getVc() {
 		return vc;
@@ -23,5 +32,11 @@ public class ViewFacade {
 	public VentanaCrear getVcr() {
 		return vcr;
 	}
+=======
+	public VentanaCrearProducto getVc() {
+		return vc;
+	}
+	
+>>>>>>> branch 'main' of https://github.com/EllDanii/MercadoMayorista.git
 
 }
